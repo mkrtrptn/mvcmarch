c8bb90ba0cb8322076959e3fe89e274c14567206
mktrptn <mktr@gmail.com>
2019-03-31 16:30:49 +0530

    Result types initial and final Commit

---
+++ mvcmarch/ResultTypes/Controllers/ResultsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResultTypes.Controllers
+{
+    public class ResultsController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult viewdemo()
+        {
+            return View();
+        }
+
+        public ContentResult contentdemo()
+        {
+            return Content("This Is Content Demo !");
+        }
+
+        public RedirectToRouteResult routedemo()
+        {
+           return RedirectToAction("Index","Results");
+        }
+
+        public FileResult filedemo()
+        {
+            return File("~/Images/25.jpg","Image/jpg","nature.jpg");
+        }
+
+        public PartialViewResult partialviewdemo()
+        {
+            return PartialView();
+        }
+
+        public RedirectResult redirectdemo()
+        {
+            return Redirect("www.google.com");
+        }
+
+    }
+}
