95690f9094fa5bcafc0e6c690f7e4f65ae85c139
mktrptn <mktr@gmail.com>
2019-03-31 14:43:28 +0530

    InitialApp1 Initial and final Commit

---
+++ mvcmarch/InitialApp1/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InitialApp1.Controllers
+{
+    public class DemoController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return Content("This Is Content Result !");
+        }
+    }
+}
---
+++ mvcmarch/InitialApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InitialApp1.MvcApplication" Language="C#" %>
