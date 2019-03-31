a5f6b236e189495592589ae087edb37d91450134
mktrptn <mktr@gmail.com>
2019-03-31 20:20:27 +0530

    cookie application initial and final

---
+++ mvcmarch/CookieApp8/Controllers/StateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieApp8.Controllers
+{
+    public class StateController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            if(Request.Cookies["val"] == null)
+            {
+                ViewBag.msg = "This Is Your First Visit Welcome";
+                Response.Cookies["val"].Value = DateTime.Now.ToString();
+                Response.Cookies["val"].Expires = DateTime.Now.AddMinutes(5);
+
+            }
+            else
+            {
+                ViewBag.msg = "Your Last Visit" + Request.Cookies["val"].Value;
+                Response.Cookies["val"].Value = DateTime.Now.ToString();
+                Response.Cookies["val"].Expires = DateTime.Now.AddMinutes(5);
+
+            }
+           
+            return View();
+        }
+    }
+}
