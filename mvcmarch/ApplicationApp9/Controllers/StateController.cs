e081f48dbfb578c3a27e487d472d6557097a0977
mktrptn <mktr@gmail.com>
2019-03-31 20:58:04 +0530

    Application Object Initial And Final

---
+++ mvcmarch/ApplicationApp9/Controllers/StateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApplicationApp9.Controllers
+{
+    public class StateController : Controller
+    {
+        public ActionResult Index()
+        {
+           return View();
+        }
+
+        public ActionResult store()
+        {
+            HttpContext.Application["comments"] = "<span>"  + HttpContext.Application["comments"].ToString() + Request.Form["cmt"] +"</span>"+ "<br/>";
+
+            return View("Index");
+        }
+    }
+}
---
+++ mvcmarch/ApplicationApp9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApplicationApp9.MvcApplication" Language="C#" %>
