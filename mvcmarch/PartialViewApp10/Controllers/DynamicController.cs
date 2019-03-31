f66a141c4d2a20bacd9be7da80de400f637206bc
mktrptn <mktr@gmail.com>
2019-03-31 23:56:36 +0530

    Dynamic Partialview Example commit 2

---
+++ mvcmarch/PartialViewApp10/Controllers/DynamicController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewApp10.Controllers
+{
+    public class DynamicController : Controller
+    {
+      
+
+        public PartialViewResult dynamicheader()
+        {
+            List<string> obj = new List<string>();
+            obj.Add("Index");
+            obj.Add("services");
+            obj.Add("contact");
+            obj.Add("about");
+
+            ViewBag.action = obj; 
+            
+            return PartialView("_DynamicHeader");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult services()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+            return View();
+        }
+
+
+
+    }
+}
