a9658f32a3151cdc1d0367eed6494b77eec63144
mktrptn <mktr@gmail.com>
2019-03-31 15:03:23 +0530

    ViewsApp Intial And Final Commit

---
+++ mvcmarch/ViewsApp2/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsApp2.Controllers
+{
+    public class DemoController : Controller
+    {
+       // Views Demo Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+    }
+}
