3e056627156471d1c06cff64b3a79d1c4886fc97
mktrptn <mktr@gmail.com>
2019-03-31 17:55:02 +0530

    Tempdata Exam[ple Initial And Final

---
+++ mvcmarch/TempdataApp6/Controllers/StateController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempdataApp6.Controllers
+{
+    public class StateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult store()
+        {
+            TempData["value"] = 1000;
+            return View("Index");
+        }
+
+        public ActionResult usepeek()
+        {
+            return View();
+        }
+
+        public ActionResult readandkeep()
+        {
+            return View();
+        }
+
+        public ActionResult normalread()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ mvcmarch/TempdataApp6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TempdataApp6.MvcApplication" Language="C#" %>
