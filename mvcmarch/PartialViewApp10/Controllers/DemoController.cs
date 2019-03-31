0ac3d9c69c81199c2b0b11a706b21a79e1f3ec13
mktrptn <mktr@gmail.com>
2019-03-31 23:23:29 +0530

    partialView initial commit

---
+++ mvcmarch/PartialViewApp10/Controllers/DemoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewApp10.Controllers
+{
+    public class DemoController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult campus()
+        {
+            return View();
+        }
+
+        public ActionResult classroom()
+        {
+            return View();
+        }
+
+        public ActionResult lab()
+        {
+            return View();
+        }
+
+        public ActionResult canteen()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
