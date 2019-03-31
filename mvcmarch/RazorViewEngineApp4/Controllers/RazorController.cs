87b3f9e7e240be731c55cbc042af85d1eedcf7bc
mktrptn <mktr@gmail.com>
2019-03-31 15:52:58 +0530

    Razor view Engine example Initial and final

---
+++ mvcmarch/RazorViewEngineApp4/Controllers/RazorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewEngineApp4.Controllers
+{
+    public class RazorController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
