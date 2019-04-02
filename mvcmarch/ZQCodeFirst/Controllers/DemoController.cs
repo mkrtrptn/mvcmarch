646efdafc74332f5ce4524271bb5a2686fe8a88d
mktrptn <mktr@gmail.com>
2019-04-03 00:07:29 +0530

    ZQ Final Commit

--- mvcmarch/ZQCodeFirst/Controllers/DemoController.cs
+++ mvcmarch/ZQCodeFirst/Controllers/DemoController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ZQCodeFirst.Models;
+using System.Data.Entity;
+using System.Net;
 
 namespace ZQCodeFirst.Controllers
 {
@@ -34,6 +36,61 @@ namespace ZQCodeFirst.Controllers
             obj.SaveChanges();
             return View("Index");
         }
+
+        public ActionResult editproduct(int id)
+        {
+            var prod = obj.products.Find(id);
+            return View(prod);
+        }
+
+        [HttpPost]
+        public ActionResult editproduct(product p)
+        {
+            product pro = obj.products.Find(p.prodid);
+            pro.prodname = p.prodname;
+            pro.price = p.price;
+            pro.quantity = p.quantity;
+           
+            //obj.Entry(pro).State = EntityState.Modified;
+            obj.SaveChanges();
+           
+            return View("Index");
+        }
+
+
+
+        public ActionResult deleteproduct(int? id)
+        {
+            if(id !=null)
+            {
+                product pro = obj.products.Find(id);
+                return View(pro);
+
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult deleteproduct(product p)
+        {
+             if( p !=null)
+            {
+                product pro = obj.products.Find(p.prodid);
+                obj.products.Remove(pro);
+                obj.SaveChanges();
+                return View("Index");
+            }
+             else
+            {
+                return View("Index");
+            }
+
+        }
+
 
 
     }
