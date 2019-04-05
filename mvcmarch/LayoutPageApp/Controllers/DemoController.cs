7f92ab7c7dc8a54c68c30011002c3871816aead3
mktrptn <mktr@gmail.com>
2019-04-05 14:26:01 +0530

    LayoutPageApp initial commit

---
+++ mvcmarch/LayoutPageApp/Controllers/DemoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutPageApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+        public ActionResult services()
+        {
+            return View();
+        }
+
+        public ActionResult tutorial()
+        {
+            return View();
+        }
+
+        public ActionResult books()
+        {
+            return View();
+        }
+
+
+
+    }
+}
