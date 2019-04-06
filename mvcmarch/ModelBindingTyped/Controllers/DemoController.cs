831f709c40484101c6455101a9504f3362964045
mktrptn <mktr@gmail.com>
2019-04-06 16:19:27 +0530

    Modelbinding Strongly typed

---
+++ mvcmarch/ModelBindingTyped/Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBindingTyped.Models;
+namespace ModelBindingTyped.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<SelectListItem> obj = new List<SelectListItem>();
+            obj.Add(new SelectListItem{ Text ="India" , Value = "India"});
+            obj.Add(new SelectListItem { Text="US" , Value="US"});
+            obj.Add(new SelectListItem { Text="Australia" , Value="Australia"});
+
+            ViewBag.country = obj;
+
+            List<string> obj2 = new List<string>();
+            obj2.Add("Angular");
+            obj2.Add("DOT NET CORE");
+            obj2.Add("Microservices");
+            obj2.Add("Azure");
+
+            ViewBag.skills = obj2;
+
+            return View();
+        }
+
+        public ActionResult store(Candidate obj)
+        {
+            obj.skills = Request.Form["skills"];
+
+            return View(obj);
+        }
+
+
+    }
+}
---
+++ mvcmarch/ModelBindingTyped/Models/Candidate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ModelBindingTyped.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ModelBindingTyped.Models
+{
+    public class Candidate
+    {
+        public string name { set; get; }
+        public string password { set; get; }
+        public string gender { set; get; }
+        public string country { set; get; }
+
+        [DataType(DataType.EmailAddress)]
+        public string email { set; get; }
+
+        public string skills { set; get; }
+
+        [DataType(DataType.Date)]
+        public string  dob { set; get; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string mobile { set; get; }
+    }
+}
