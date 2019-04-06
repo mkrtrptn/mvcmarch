c400c91e7c829a271aa28c45174f1f384099d467
mktrptn <mktr@gmail.com>
2019-04-06 14:26:03 +0530

    Modelbinding untyped

---
+++ mvcmarch/ModelbindingUntyped/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelbindingUntyped.Models;
+namespace ModelbindingUntyped.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult store(UserModel u)
+        {
+            return View(u);
+        }
+
+
+    }
+
+
+}
