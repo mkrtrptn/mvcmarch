25cc769cc10464f0595c9bf69a98dc90ff178fb3
mktrptn <mktr@gmail.com>
2019-04-06 13:36:51 +0530

    Untyped Helper Controll Commit

---
+++ mvcmarch/UntyedHelperControl/Controllers/DemoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UntyedHelperControl.Models;
+
+namespace UntyedHelperControl.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult store()
+        {
+            string amenities = "";
+
+            if (Request.Form["c1"] != "false")
+                amenities = "wifi";
+            if (Request.Form["c1"] != "false")
+                amenities = amenities + "AC";
+
+
+            CustomerModel cobj = new CustomerModel
+            {
+                custname = Request.Form["txtname"],
+                roomtype = Request.Form["room"],
+                amenities = amenities 
+                
+            };
+
+
+
+            return View(cobj);
+        }
+
+    }
+}
---
+++ mvcmarch/UntyedHelperControl/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UntyedHelperControl.Models
+{
+    public class CustomerModel
+    {
+        public string custname { set; get; }
+        public string roomtype { set; get; }
+        public string amenities { set; get; }
+    }
+}
