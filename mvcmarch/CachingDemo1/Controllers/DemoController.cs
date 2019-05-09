495833a119a70e2f1a9af5cc2dc8d2effd9497ae
mktrptn <mktr@gmail.com>
2019-05-09 10:32:12 +0530

    cachingApp1

---
+++ mvcmarch/CachingDemo1/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CachingDemo1.Models;
+
+namespace CachingDemo1.Controllers
+{
+    public class DemoController : Controller
+    {
+        InventoryEntities db = new InventoryEntities();
+
+        [OutputCache(Duration =180,Location = System.Web.UI.OutputCacheLocation.Any)]
+        public ActionResult Index()
+        {
+            return View(db.products.ToList());
+
+       
+        }
+    }
+}
